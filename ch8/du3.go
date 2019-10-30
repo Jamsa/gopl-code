ac35133991f1b49d2c3d0493ea4d8512ab4284ec
朱杰 <zhujie@zhujiedeMacBook-Pro.local>
2019-10-30 20:28:32 +0800

    增加利用time.Time定时输出明细信息的du2.go 增加并行遍历目录和利用channel buffer限定并行数量的du3.go

--- ch8/du3.go
+++ ch8/du3.go
@@ -1,5 +1,5 @@
-//支持v参数的每500ms显示明细信息的 du 版本
-
+// 增加并行的遍历目录的功能，进一步提高运行速度
+// 通过sync.WaitGrou对并行数量计数
 package main
 
 import (
@@ -8,26 +8,35 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
 //目录遍历
-func walkDir(dir string, fileSizes chan<- int64) {
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
+			n.Add(1)
 			subdir := filepath.Join(dir, entry.Name())
-			walkDir(subdir, fileSizes)
+			walkDir(subdir, n, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
 	}
 }
 
+// 程序高峰期可能会创建成百上千的goroutine
+// sema 是用于限制并发数量的计数器，利用channel buffer
+var sema = make(chan struct{}, 20)
+
 //获取目录内容
 func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}        // 获取计数
+	defer func() { <-sema }() //释放计数
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "du2: %v\n", err)
+		fmt.Fprintf(os.Stderr, "du3: %v\n", err)
 		return nil
 	}
 	return entries
@@ -49,10 +58,14 @@ func main() {
 	}
 
 	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes) //并行遍历目录
+	}
+
 	go func() {
-		for _, root := range roots {
-			walkDir(root, fileSizes)
-		}
+		n.Wait()
 		close(fileSizes)
 	}()
 
