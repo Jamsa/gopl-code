fef426ee8fb04c664e2abf562ac2c8e7b9487a9e
朱杰 <zhujie@zhujiedeMacBook-Pro.local>
2019-11-05 08:58:25 +0800

    增加ch12反射相关的示例

---
+++ ch12/methods.go
@@ -0,0 +1,25 @@
+// 反射获取类型的所有方法
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
+
+func Print(x interface{}) {
+	v := reflect.ValueOf(x)
+	t := v.Type()
+	fmt.Printf("type %s\n", t)
+	for i := 0; i < v.NumMethod(); i++ {
+		methType := v.Method(i).Type()
+		fmt.Printf("func (%s) %s%s\n", t, t.Method(i).Name,
+			strings.TrimPrefix(methType.String(), "func"))
+	}
+}
+
+func main() {
+	Print(time.Hour)
+	Print(new(strings.Replacer))
+}
